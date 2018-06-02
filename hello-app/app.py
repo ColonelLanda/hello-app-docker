eb2e2babea9126c6ba5fae9e2b36051e4693561e
Itamar Ostricher <itamarost@gmail.com>
2018-06-02 18:40:53 +0300

    Add hello-app code & Dockerfile

---
+++ hello-app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+# Connect to Redis
+redis = Redis(host=os.getenv('REDIS_HOST', 'redis'),
+              password=os.getenv('REDIS_PASSWORD'),
+              socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route('/healthz')
+def health_check():
+    return 'OK'
+
+@app.route('/')
+def hello():
+    try:
+        visits = redis.incr('counter')
+    except RedisError as ex:
+        app.logger.error('Could not connect to redis: %s', ex)
+        visits = '<i>cannot connect to Redis, counter disabled</i>'
+
+    html = """
+<h3>Hello {name}!</h3>
+<b>Hostname:</b> {hostname}<br/>
+<b>Visits:</b> {visits}
+"""
+    return html.format(name=os.getenv('NAME', 'world'),
+                       hostname=socket.gethostname(),
+                       visits=visits)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
